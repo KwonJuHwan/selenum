2f9e4114f273c7a9e13c5791c19f79b49708fef9
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-12-23 20:14:55 +0900

    [init] 처음 Url에서 url 타고 들어가는 것까지 구현

--- src/main/java/com/seleniumtest/SeleniumTestApplication.java
+++ src/main/java/com/seleniumtest/SeleniumTestApplication.java
@@ -14,23 +14,38 @@ import org.springframework.util.ObjectUtils;
 @SpringBootApplication
 public class SeleniumTestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver chromeDriver = WebDriverUtil.getChromeDriver();
         WebElement title;
-        String url = "https://linkareer.com/activity/159112";
-        String query = "crossorigin";
+        String detailUrl = "https://linkareer.com/activity/159112";
+        String listUrl = "https://linkareer.com/list/activity";
 
-        if (!ObjectUtils.isEmpty(chromeDriver)) {
-            chromeDriver.get(url);
-            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-            title = chromeDriver.findElement(By.className("title"));
-            System.out.println(title.getText());
-            WebElement element = chromeDriver.findElement(
-                By.xpath("//*[@id=\"__next\"]/div[1]/div/main/div/article/div[1]/div/div/h1"));
-            System.out.println(element.getText());
-            chromeDriver.quit();
+        chromeDriver.get(listUrl);
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        List<WebElement> elements = chromeDriver.findElements(By.className("image-link"));
+        WebElement titleLink = elements.get(0);
+        titleLink.click();
 
-        }
+        Thread.sleep(1000);
+
+        String currentUrl = chromeDriver.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        title = chromeDriver.findElement(By.className("title"));
+        System.out.println(title.getText());
+        chromeDriver.quit();
+
+//        if (!ObjectUtils.isEmpty(chromeDriver)) {
+//            chromeDriver.get(detailUrl);
+//            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//            title = chromeDriver.findElement(By.className("recruitText"));
+//            System.out.println(title.getText());
+//            WebElement element = chromeDriver.findElement(
+//                By.xpath("//*[@id=\"__next\"]/div[1]/div/main/div/article/div[1]/div/div/h1"));
+//            System.out.println(element.getText());
+//            chromeDriver.quit();
+//
+//        }
 
     }
 
